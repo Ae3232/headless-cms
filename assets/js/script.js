2350c37023507d036ef83ab007bed6d32271a658
Yurov, Roman <ryurov@scnsoft.com>
2021-11-23 22:02:37 +0300

    update front

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,17 @@
-$(document).ready(function() {
-	$("#testimonials").slick({
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 4000
+(() => {
+	document.addEventListener('DOMContentLoaded', () => {
+		const btnToolTip = document.querySelector('.header__sandwich');
+		const toolTipBtnClose = document.querySelector('.icon-close');
+		const navDropDown = document.querySelector('.navigation');
+		const overlay = document.querySelector('.overlay');
+
+		btnToolTip.addEventListener('click', e => {
+			navDropDown.classList.toggle('_open');
+			overlay.classList.toggle('_open');
+		});
+		toolTipBtnClose.addEventListener('click', e => {
+			navDropDown.classList.remove('_open');
+			overlay.classList.remove('_open');
+		});
 	});
-});
+})();
